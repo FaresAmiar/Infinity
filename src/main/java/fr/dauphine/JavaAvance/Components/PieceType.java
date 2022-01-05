c3dbee919e717f28ebacacb5e3bbc38d8e63f4ff
FaresAmiar <45738287+FaresAmiar@users.noreply.github.com>
2022-01-05 23:04:29 +0100

    correction fonc

--- src/main/java/fr/dauphine/JavaAvance/Components/PieceType.java
+++ src/main/java/fr/dauphine/JavaAvance/Components/PieceType.java
@@ -15,45 +15,33 @@ public enum PieceType {
     VOID, ONECONN, BAR, TTYPE, FOURCONN, LTYPE;
     // Each Type has a number of connectors and a specific value
 
-
     public static PieceType getTypefromValue(int typeValue) {
-        return typeValue == 0 ? PieceType.VOID : (
-            typeValue == 1 ? PieceType.ONECONN : (
-                typeValue == 2 ? PieceType.BAR : (
-                    typeValue == 3 ? PieceType.TTYPE : (
-                        typeValue == 4 ? PieceType.FOURCONN : (
-                            typeValue == 5 ? PieceType.LTYPE : null
-                        )
-                    )
-                )
-            )
-        );
+        return typeValue == 0 ? PieceType.VOID
+                : (typeValue == 1 ? PieceType.ONECONN
+                        : (typeValue == 2 ? PieceType.BAR
+                                : (typeValue == 3 ? PieceType.TTYPE
+                                        : (typeValue == 4 ? PieceType.FOURCONN
+                                                : (typeValue == 5 ? PieceType.LTYPE : null)))));
     }
 
     public int getNbConnectors() {
-        return this == VOID ? 0 : (
-            this == ONECONN ? 1 : (
-                this == BAR ? 2 : (
-                    this == TTYPE ? 3 : (
-                        this == FOURCONN ? 4 : (
-                            this == LTYPE ? 2 : -1
-                        )
-                    )
-                )
-            )
-        );
-        
+        return this == VOID ? 0
+                : (this == ONECONN ? 1
+                        : (this == BAR ? 2 : (this == TTYPE ? 3 : (this == FOURCONN ? 4 : (this == LTYPE ? 2 : -1)))));
+
     }
 
-    /*public ArrayList<Orientation> getListOfPossibleOri() {
+    public ArrayList<Orientation> getListOfPossibleOri() {
         List<Orientation> empty = Arrays.asList();
-        List<Orientation> oneconn = Arrays.asList(Orientation.NORTH, Orientation.EAST, Orientation.SOUTH, Orientation.WEST);
+        List<Orientation> oneconn = Arrays.asList(Orientation.NORTH, Orientation.EAST, Orientation.SOUTH,
+                Orientation.WEST);
         List<Orientation> bar = Arrays.asList(Orientation.NORTH, Orientation.EAST);
-        List<Orientation> ttype = Arrays.asList(Orientation.NORTH,)
-        return this == VOID ? empty : (
-            this == ONECONN ? Arrays.asList(Orientation.)
-        )
-    }*/
+        return null;
+        /*
+         * List<Orientation> ttype = Arrays.asList(Orientation.NORTH,) return this ==
+         * VOID ? empty : ( this == ONECONN ? Arrays.asList(Orientation.) )
+         */
+    }
 
     public Orientation getOrientation(Orientation orientation) {
         return orientation.getOrientation();
@@ -62,4 +50,5 @@ public enum PieceType {
     public LinkedList<Orientation> setConnectorsList(Orientation orientation) {
         return null;
     }
+
 }
